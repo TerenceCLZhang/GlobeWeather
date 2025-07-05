14052847fba46dd9077f76e988c27aa57ae1e687
TerenceCLZhang <terence.zhang2014@gmail.com>
2025-07-05 12:49:14 +1200

    refactored

--- src/state/LocationSlice.tsx
+++ src/state/LocationSlice.tsx
@@ -13,12 +13,8 @@ export const LocationSlice = createSlice({
   name: "location",
   initialState,
   reducers: {
-    setLocation: (state, action: PayloadAction<LocationInterface>) => {
-      state.lat = action.payload.lat;
-      state.lon = action.payload.lon;
-      state.name = action.payload.name;
-      state.state = action.payload.state;
-      state.country = action.payload.country;
+    setLocation: (_, action: PayloadAction<LocationInterface>) => {
+      return action.payload;
     },
   },
 });
